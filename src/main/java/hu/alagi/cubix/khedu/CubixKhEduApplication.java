933d9ce7143a6b73c03254f9ae5b4b1ccc4187e2
Varga Tamás <varga.tamas@vtep.videoton.hu>
2023-06-06 17:02:39 +0200

    Init commit

---
+++ src/main/java/hu/alagi/cubix/khedu/CubixKhEduApplication.java
@@ -0,0 +1,13 @@
+package hu.alagi.cubix.khedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubixKhEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CubixKhEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/alagi/cubix/khedu/CubixKhEduApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.alagi.cubix.khedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubixKhEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
